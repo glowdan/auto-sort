733b61ccad53ad23baf68c7ee4f4eb64ba2d6bb7
Glowdan <glowdan@gmail.com>
2022-10-11 11:32:34 +0800

    add dynamic steps

--- src/main/dev/glowdan/AutoSort.java
+++ src/main/dev/glowdan/AutoSort.java
@@ -1,5 +1,6 @@
 package dev.glowdan;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,7 +21,7 @@ public class AutoSort {
     public static <T extends Named> List<T> sort(List<T> originList) {
         return originList.stream()
                 .map(one -> new SortStruct<>(SortScoreMap.getOrDefault(one.getName(), 0.0f), one))
-                .sorted()
+                .sorted((Comparator.comparing(o -> o.score)))
                 .map(SortStruct::getValue)
                 .collect(Collectors.toList());
     }
@@ -34,7 +35,7 @@ public class AutoSort {
         stat.update(timeCost);
     }
 
-    private static class SortStruct<T> implements Comparable<SortStruct<T>> {
+    private static class SortStruct<T> {
         Float score;
         T value;
 
@@ -47,16 +48,13 @@ public class AutoSort {
             return value;
         }
 
-        @Override
-        public int compareTo(SortStruct o) {
-            return o.score.compareTo(this.score);
-        }
     }
 
     private static class LoaderStat {
         private int preCount = 0;
         private long preCost = 0;
         private final String loaderName;
+        private int updateSteps = 1;
 
         public LoaderStat(String loaderName) {
             this.loaderName = loaderName;
@@ -65,10 +63,14 @@ public class AutoSort {
         public synchronized void update(long timecost) {
             preCost += timecost;
             preCount++;
-            if (preCount > 50) { //大概半分到1分钟
+            if (preCount > updateSteps) { //大概半分到1分钟
                 SortScoreMap.put(loaderName, 1f * preCost / preCount);
                 preCount = 0;
                 preCost = 0;
+                updateSteps *= 2;
+                if (updateSteps > 1024) {
+                    updateSteps = 1024;
+                }
             }
         }
 
